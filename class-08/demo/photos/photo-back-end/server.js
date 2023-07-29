f3459822ad86782da4c63dbfe40c5544bbfaac27
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-07-29 17:20:23 +0800

    remove route from backend

--- class-08/demo/photos/photo-back-end/server.js
+++ class-08/demo/photos/photo-back-end/server.js
@@ -11,30 +11,11 @@ app.use(cors());
 
 const PORT = process.env.PORT;
 
-app.get('/photos', getPhotos);
+// We need a route to handle requests like this:
+// http://localhost:3001/photos?searchQuery=smoothies
 
-async function getPhotos(request, response) {
-  const searchQuery = request.query.searchQuery;
 
-  const url = `https://api.unsplash.com/search/photos/?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=${searchQuery}`;
 
-  try {
-    const photosResponse = await axios.get(url);
-    const photoArray = photosResponse.data.results.map(photo => new Photo(photo));
-    response.status(200).send(photoArray);
-  } catch (err) {
-    console.error('error from superagent', err);
-    response.status(500).send('server error');
-  }
-}
-
-class Photo {
-  constructor(obj) {
-    this.img_url = obj.urls.regular;
-    this.original_image = obj.links.self;
-    this.photographer = obj.user.name;
-  }
-}
 
 app.get('*', notFound);
 
