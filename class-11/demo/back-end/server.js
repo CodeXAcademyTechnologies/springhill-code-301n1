e5c741f94a4baa6280172ff7979524a7d14d1024
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-08-15 16:51:48 +0800

    comments on mongoose

--- class-11/demo/back-end/server.js
+++ class-11/demo/back-end/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+// import our odm
+// Object-Document-Mapper
 const mongoose = require('mongoose');
+
 const cors = require('cors');
 require('dotenv').config();
 const Cat = require('./models/cat');
@@ -10,6 +13,10 @@ const app = express();
 
 app.use(cors());
 
+// Our server maintains a connection to the database
+// Mongoose can take care of the DB connection for us.
+// Just use this line once in your code, and mongoose
+// will be connected.
 mongoose.connect(process.env.DATABASE_URL);
 
 app.get('/cats', async (request, response) => {
